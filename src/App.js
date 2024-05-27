f33b4cb44400a837f741c34806c09ec37ea6a1bb
Diogo Martins <diogobarrosmartins@hotmail.com>
2024-05-27 06:32:55 +0100

    add team
    problem with MuiDataGrid-topContainer color

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-
-// import Team from ".*scenes/team";
+import Team from "./scenes/team";
 // import Invoices from ".*scenes/invoices";
 // import Contacts from ".*scenes/dashboard";
 // import Bar from ".*scenes/dashboard";
@@ -44,7 +43,7 @@ function App() {
           <Topbar setIsSidebar={setIsSidebar}/>
             <Routes>
               <Route path ="/" element ={<Dashboard/>} />
-              {/* <Route path ="/team" element ={<Team/>} /> */}
+               <Route path ="/team" element ={<Team/>} /> 
               {/* <Route path ="/contacts" element ={<Contacts/>} /> */}
               {/* <Route path ="/invoices" element ={<Invoices/>} /> */}
               {/* <Route path ="/form" element ={<Form/>} /> */}
