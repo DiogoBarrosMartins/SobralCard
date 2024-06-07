69cda8541cc308acd12ff7fee9abf76f05b26fcb
Diogo Martins <diogobarrosmartins@hotmail.com>
2024-06-07 22:14:25 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,8 +7,7 @@ const Card = ({ card }) => {
     <div>
       <Typography variant="h4">{card.name}</Typography>
       {card.card && <img src={card.card} alt={card.name} style={{ maxWidth: '100%', borderRadius: "25px" }} />}
-      <Typography variant="body1">Type: {card.type_line}</Typography>
-      <Typography variant="body1">Rarity: {card.rarity}</Typography>
+      
       {card.prices && (
         <Typography variant="body1">Price (Euros): {card.prices}</Typography>
       )}
