c187510286dc8bdbe93d8cdc4c2087d0cc5385db
Diogo Martins <diogobarrosmartins@hotmail.com>
2024-06-07 23:01:26 +0100

    update card prices

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,10 +7,12 @@ const Card = ({ card }) => {
       <Typography variant="h4">{card.name}</Typography>
       {card.card && <img src={card.card} alt={card.name} style={{ maxWidth: '100%', borderRadius: "25px" }} />}
       
-      {card.prices && (
-        <Typography variant="body1">Price (Euros): {card.prices}</Typography>
-      )}
-     
+      {card.prices ? (
+  <Typography variant="body1">Price: {card.prices}€</Typography>
+) : (
+  <Typography variant="body1">Priceless</Typography>
+)}
+
     </div>
   );
 };
