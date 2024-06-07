c08b68818129c57f8b77c3e9bc345201e5df88bd
Diogo Martins <diogobarrosmartins@hotmail.com>
2024-06-07 02:22:07 +0100

    add faq

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import Form from "./scenes/form";
-// import Faq from "./scenes/faq";
+import Faq from "./scenes/faq";
 // import Geography from ".*scenes/geography";
- import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 /**
  * O ColorModeContext.Provider  vai dar-me um provider de valor:{colorMode}
@@ -39,20 +39,19 @@ function App() {
       <div className="app">
         <Sidebar isSidebar={isSidebar} />
         <main className="content">
-       
           <Topbar setIsSidebar={setIsSidebar}/>
             <Routes>
               <Route path ="/" element ={<Dashboard/>} />
-               <Route path ="/team" element ={<Team/>} /> 
-               <Route path ="/contacts" element ={<Contacts/>} /> 
-               <Route path ="/invoices" element ={<Invoices/>} /> 
-               <Route path ="/form" element ={<Form/>} /> 
+              <Route path ="/team" element ={<Team/>} /> 
+              <Route path ="/contacts" element ={<Contacts/>} /> 
+              <Route path ="/invoices" element ={<Invoices/>} /> 
+              <Route path ="/form" element ={<Form/>} /> 
+              <Route path ="/faq" element ={<Faq/>} /> 
+              <Route path ="/calendar" element ={<Calendar/>} />
               {/* <Route path ="/bar" element ={<Dashboard/>} /> */}
               {/* <Route path ="/pie" element ={<Dashboard/>} /> */}
               {/* <Route path ="/line" element ={<Dashboard/>} /> */}
-              {/* <Route path ="/faq" element ={<Dashboard/>} /> */}
               {/* <Route path ="/geography" element ={<Geography/>} /> */}
-               <Route path ="/calendar" element ={<Calendar/>} />
             </Routes>
         </main>
       </div>
