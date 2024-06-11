c9e2176b400aaac882edf3458a56c1a7920ea14f
Diogo Martins <diogobarrosmartins@hotmail.com>
2024-06-11 01:04:01 +0100

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState , useRef } from "react";
+import { useState, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -9,6 +9,7 @@ import CardScene from "./scenes/cardscene";
 import ListMenuScene from "./scenes/listmenuscene";
 import CardSearchScene from "./scenes/listscene";
 import ListCardDetailScene from "./scenes/listcarddetailscene";
+
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -17,15 +18,16 @@ function App() {
 
   const updateSidebar = () => {
     if (updateSidebarRef.current) {
-        updateSidebarRef.current();
+      updateSidebarRef.current();
     }
-};
+  };
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-        <Sidebar storedLists={storedLists} updateSidebarRef={updateSidebarRef} />
+          {isSidebar && <Sidebar storedLists={storedLists} updateSidebarRef={updateSidebarRef} />}
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
